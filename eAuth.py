df5332e48edfe375014929b9fb6b9f26e6edc0b4
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2022-10-03 14:38:35 +0530

    Numerous bug fixes.

--- eAuth.py
+++ eAuth.py
@@ -1,12 +1,14 @@
 # imports
-import pickle, caesarCipher
-
+import pickle, caesarCipher, csv
+from election import *
 #---------------------------------------------------------Logins----------------------------------------------------------
 
 def adminLogin(): #Admin login details check
+    print("")
+    print("Log in as administrator")
     adminName = input("Admin Name: ")
     adminPassword = input("Password: ")
-    f = open("cred.dat", "rb")
+    f = open("Data/cred.dat", "rb")
     try:
         while True:
             data = pickle.load(f)
@@ -19,17 +21,22 @@ def adminLogin(): #Admin login details check
 
 
 def voterLogin(): #Checks whether the VOTER'S name and UID exists within the database and correspond to each other
-    name = input("Name: ")
+    print("")
+    print("Log in as voter")
     ID = input("ID: ")
-    votersFile = open("voterList.dat", "rb")
-    try:
-        while True:
-            voterData = pickle.load(votersFile)
-            if voterData["Name"] == name and voterData["ID"] == ID:
-                votersFile.close()
-                return True
-    except EOFError:
-        votersFile.close() 
-        return False
+    name = input("Name: ")
+    
+    if ID == "EXIT" or name == "EXIT":
+        return "EXIT"
+        
+    f = open("Data/voterList.csv", 'r', newline="")
+    reader = csv.reader(f)
+    
+    for i in reader:
+        if ID == i[0] and name == i[1] and hasVoted(ID) == False:
+            f.close()
+            return True
+    f.close()
+    return False
 
 #^-------------------------------------------------------^Logins^--------------------------------------------------------^
