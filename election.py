1e835a77e40d38f26c5100c096d438c0ef15e85c
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2022-10-04 13:43:21 +0530

    Updated code with proper imports.

--- election.py
+++ election.py
@@ -1,8 +1,7 @@
 # imports
 import datetime, pickle, uuid, csv
-from pprint import pprint
-from helper import *
-from eAuth import *
+import helper
+import eAuth
 
 #----------------------------------------------------------Menus----------------------------------------------------------
 
@@ -37,14 +36,14 @@ def elecSettings(admin):
     boothNo = input("Booth number: ")
     sessionID = str(uuid.uuid4()).split("-")[0]
     
-    if confirm():
+    if helper.confirm():
         voteCount = [["Candidate ID", "Candidate Name", "Votes"]]
         settingsFile = open("Data/settings.dat", "ab")
         timeStamp = str(datetime.datetime.now())
-        settings = {"Session ID":sessionID, "Time": timeStamp, "Election Officer":admin,"Post":post,"Booth Number":boothNo}
+        settings = {"Session ID":sessionID, "Time": timeStamp, "Election Officer":admin, "Post":post, "Booth Number":boothNo}
         pickle.dump(settings,settingsFile)
         
-        for i in fetchCandidates()[1:]:
+        for i in helper.fetchCandidates()[1:]:
             voteCount.append([i[0],i[1],0])
         
         with open(f"Data/voteCount-{sessionID}.csv", "a", newline="") as voteCountFile:
@@ -62,30 +61,30 @@ def elecSess(sessionID, settings,voteCount): #Starts a election session using an
             r_o = csv.reader(voteCountFile)
             for i in r_o: voteCount.append(i)
 
-    if voterLogin(): #Login check for voter
+    if eAuth.voterLogin(): #Login check for voter
         print("Candidate List:")
-        print(displayCandidates())
-        print("")
-        post = settings["Post"]
+        helper.displayCandidates()
+        print("") #Prints an empty line after the candidate list is printed
+        post = settings["Post"] #Settings recieved from main from input
         print(f"Aforementioned table is the list of candidates for the election standing for {post}.\nYou may pick the candidate of your choice by submitting the unique id associated with the candidate (see candidate list — first column)")
         
-        data = fetchCandidates()
+        data = helper.fetchCandidates() #Gets candidate data from candidateList.csv
 
         while True:
             ch = input("Your choice: ")
             if ch == "EXIT":
                 print("Saving session...")
-                return (False, voteCount, False)
+                return (False, voteCount, False) #(Has voted - False, voteCount List, Exit loop) 
             
-            for i in data[1:]:
+            for i in data[1:]: #Ignoring heading of csv file and iterating through rest
                 if ch == i[0]:
                     print(f"You have chosen to vote for {i[1]}")
                     # confirming vote
-                    if confirm():
-                        voteCount = vote(i[0], voteCount)
+                    if helper.confirm():
+                        voteCount = vote(i[0], voteCount) #Accepts the candidate id and votecount and increments voteCount by 1
                         print("Vote casted successfully!")
                         # print(voteCount)
-                        return (True, voteCount, False)
+                        return (True, voteCount, False) #(Has voted - True, voteCount List, Exit loop)
                     else:
                         print("Recast your vote!")
                         pass
@@ -93,7 +92,7 @@ def elecSess(sessionID, settings,voteCount): #Starts a election session using an
                 print("Candidate ID does not exist! Check the candidate list's ID column carefully and vote again")
     else:
         print("Incorrect voter credentials!")
-        return(False, voteCount, True)
+        return(False, voteCount, True) #(Has voted - False, voteCount List, Continue loop) 
 
 def vote(choiceID, voteCount):
     for i in voteCount:
@@ -107,7 +106,7 @@ def saveSession(sessionID, voteCount):
             w_o.writerows(voteCount)
 
 def hasVoted(ID):
-    voterData = fetchVoters()
+    voterData = helper.fetchVoters()
     for i in voterData[1:]:
         if i[0] == ID and i[4] == "Y":
             return True
